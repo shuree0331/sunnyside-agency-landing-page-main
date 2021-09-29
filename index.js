23fdd4e8fc60636eaf4fff2bb2b4ffe30f5a962b
Shuree <shuree0331@gmail.com>
2021-09-29 21:36:51 +1000

    test menu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const menu = document.getElementById("menu");
 const links = document.getElementById("links");
 
 menu.addEventListener('click', function () {
+    alert('boo')
     // console.log('hjhkhkjhkjhkj')
     if (links.style.visibility === 'hidden') {
         links.style.visibility = 'visible';
