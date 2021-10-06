92262c53f9a078bd8fbe1f8fd98018d45d972093
Shuree <shuree0331@gmail.com>
2021-10-06 21:04:17 +1100

    fix safari version

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const menu = document.getElementById("menu");
 const links = document.getElementById("links");
-let clickOrTouchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
-menu.addEventListener(clickOrTouchEvent, function () {
+menu.addEventListener("click", function () {
     if (links.style.visibility === 'hidden') {
         links.style.visibility = 'visible';
     } else {
